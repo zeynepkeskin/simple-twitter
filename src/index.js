4b64c276eafbcdba367d79c92de1f81b94c5ee9c
Zeynep Keskin <zeynepkeskin10@gmail.com>
2023-06-11 17:27:09 -0700

    added header

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div className='header'>
+      <a href='/'>
+        <img src='/img/logo.png'/>
+        Twitter
+      </a>
+    </div>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
